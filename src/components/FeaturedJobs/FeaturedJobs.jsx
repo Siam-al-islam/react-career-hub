416ff766534aeb0b5a2deeca294eef08d6e2a94b
Siam-al-islam <siamalislam692@gmail.com>
2024-05-19 20:26:47 +0600

    categoty list added

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,11 @@
+const FeaturedJobs = () => {
+    return (
+        <div className="max-w-[650px] mx-auto mt-10">
+            <h2 className='text-5xl font-bold text-center'>Featured Jobs</h2>
+            <p className="mt-4 text-center">Explore thousands of job opportunities with all the information you need. Its your future
+            </p>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
 
 const Home = () => {
     return (
         <div className="max-w-[1170px] mx-auto">
             <Banner />
+            <CategoryList />
+            <FeaturedJobs />
         </div>
     );
 };
